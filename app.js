8890dadaa17341cd5fb4edc526ec5f70b69be935
ElvisLC <elvislacruz22@gmail.com>
2024-07-08 02:29:11 -0400

    portfolio

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use("/", express.static(path.resolve("views/main")))
 app.use("/assets", express.static(path.resolve("views/assets")))
 app.use("/src", express.static(path.resolve("views/src")))
+app.use("/views", express.static(path.resolve("views")))
+
 
 
 module.exports=app
