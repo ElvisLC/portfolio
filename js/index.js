e30bf5f132a1de897abe0aea759de61c42d4cb3d
ElvisLC <elvislacruz22@gmail.com>
2024-07-09 23:38:38 -0400

    portfolio

--- js/index.js
+++ js/index.js
@@ -14,46 +14,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // send the message and get a callback with an error or details of the message that was sent
-
-const formBtn=document.querySelector('.form')
-const user=document.querySelector('#name')
-const email=document.querySelector('#email')
-const message=document.querySelector('#message')
-const alertM=document.querySelector('.alert')
-const recaptchaResponse = grecaptcha.getResponse();
-console.log(recaptchaResponse);
-
+const formBtn = document.querySelector('.form');
+const user = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+const alertM = document.querySelector('.alert');
 
 formBtn.addEventListener('submit', (e) => {
-    e.preventDefault()
-    if (!user.value ||!email.value ||!message.value) {
-        if (!user.value) user.classList.add('error')
-        if (!email.value) email.classList.add('error')
-        if (!message.value) message.classList.add('error')
-        if (!recaptchaResponse) alert('Por favor, completa el CAPTCHA');
-    //   alert('Todos los campos son obligatorios')
-   
-    } else {
-        
-      emailjs.send("elvisemail", "elvistemplate", {
-        from_name: `${user.value}`,
-        from_email: `${email.value}`,
-        to_name: "Elvis",
-        message: `${message.value}`,
-      });
-  
-      if (emailjs) {
-        alertM.classList.add('slideDown');
-        setTimeout(() => {
-          alertM.classList.remove('slideDown');
-          alertM.addEventListener('animationend', () => {
-            alertM.remove(); /* Elimina el elemento cuando termina la animación de salida */
-          });
-          alertM.classList.add('slideOut');
-        }, 3000);
-      }
-    }
-  });
+  e.preventDefault();
+
+  const recaptchaResponse = grecaptcha.getResponse(); // Obtener la respuesta del CAPTCHA
+
+  if (!user.value || !email.value || !message.value || !recaptchaResponse) {
+    if (!user.value) user.classList.add('error');
+    if (!email.value) email.classList.add('error');
+    if (!message.value) message.classList.add('error');
+    if (!recaptchaResponse) alert('Por favor, completa el CAPTCHA');
+  } else {
+    emailjs.send("elvisemail", "elvistemplate", {
+      from_name: `${user.value}`,
+      from_email: `${email.value}`,
+      to_name: "Elvis",
+      message: `${message.value}`,
+    }).then(() => {
+      alertM.classList.add('slideDown');
+      setTimeout(() => {
+        alertM.classList.remove('slideDown');
+        alertM.addEventListener('animationend', () => {
+          alertM.remove(); /* Elimina el elemento cuando termina la animación de salida */
+        });
+        alertM.classList.add('slideOut');
+      }, 3000);
+    });
+  }
+});
+
 
   user.addEventListener('change', () => {
     if (!user.value) user.classList.add('error')
