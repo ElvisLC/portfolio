7084c738acbf4c33df6ed00ccdb9a8a8cfbe92a3
ElvisLC <elvislacruz22@gmail.com>
2024-07-09 21:29:56 -0400

    portfolio

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
     const navBar = document.querySelector('.navBar');
     if (navBar) {
@@ -11,3 +10,59 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+
+// send the message and get a callback with an error or details of the message that was sent
+
+const formBtn=document.querySelector('.form')
+const user=document.querySelector('#name')
+const email=document.querySelector('#email')
+const message=document.querySelector('#message')
+const alertM=document.querySelector('.alert')
+formBtn.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (!user.value ||!email.value ||!message.value) {
+        if (!user.value) user.classList.add('error')
+        if (!email.value) email.classList.add('error')
+        if (!message.value) message.classList.add('error')
+    //   alert('Todos los campos son obligatorios')
+   
+    } else {
+      emailjs.send("elvisemail", "elvistemplate", {
+        from_name: `${user.value}`,
+        from_email: `${email.value}`,
+        to_name: "Elvis",
+        message: `${message.value}`,
+      });
+  
+      if (emailjs) {
+        alertM.classList.add('slideDown');
+        setTimeout(() => {
+          alertM.classList.remove('slideDown');
+          alertM.addEventListener('animationend', () => {
+            alertM.remove(); /* Elimina el elemento cuando termina la animación de salida */
+          });
+          alertM.classList.add('slideOut');
+        }, 3000);
+      }
+    }
+  });
+
+  user.addEventListener('change', () => {
+    if (!user.value) user.classList.add('error')
+    else user.classList.remove('error')
+  });
+  
+  email.addEventListener('change', () => {
+    if (!email.value) email.classList.add('error')
+    else email.classList.remove('error')
+  });
+  
+  message.addEventListener('input', () => {
+    message.classList.remove('error')
+  });
+  
+  message.addEventListener('blur', () => {
+    if (!message.value) message.classList.add('error')
+  });
