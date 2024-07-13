5bf64c9f4ec06cec4671e6b48c377200820a12ca
ElvisLC <elvislacruz22@gmail.com>
2024-07-12 23:23:24 -0400

    portfolio

--- js/index.js
+++ js/index.js
@@ -113,3 +113,12 @@ flagsElement.addEventListener('click',(e)=>{
   }
 
 })
+
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "50%";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
